f4bbe22c3d9f24dfd5c8691fd825f50f709ec5cf
Andrij-Maksymyshyn <84930890+Andrij-Maksymyshyn@users.noreply.github.com>
2021-11-22 13:46:27 +0200

    add  SimpleLightbox & scroll

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import { Notify } from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import './css/styles.css';
 
 const search_form = document.querySelector('#search-form');
@@ -25,13 +27,13 @@ search_form.addEventListener('submit', (e) => {
      load_more.classList.add('is-hidden');        
      return;
      };
-fetchPicture(value).then(showPictures);
 
+fetchPicture(value).then(showPictures);
 page += 1;
 
  load_more.classList.remove('is-hidden');
 
-
+ 
  if ([value] !== [value]) {
   gallery.innerHTML = '';
    }
@@ -41,7 +43,24 @@ page += 1;
   input.addEventListener('input', () => {
     page = 1;
   }) 
+
+
+
+  load_more.addEventListener('click', () => {
+    
+    const value = search_form.elements.searchQuery.value;
+      fetchPicture(value).then(showPictures);
      
+      page += 1;
+
+        
+      if (page > totalPages) {
+       load_more.classList.add('is-hidden');
+       Notify.info("We're sorry, but you've reached the end of search results.");
+      }
+      });
+  
+           
 //////////////////////////////////////////////////////////////////////////
 
 async function fetchPicture (value) {
@@ -55,61 +74,74 @@ async function fetchPicture (value) {
     };
 
    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-function showPictures(data) {
+   function showPictures(data) {
    
-  if (data.total === 0) {
-    load_more.classList.add('is-hidden');
-    return Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    if (data.total === 0) {
+      load_more.classList.add('is-hidden');
+      return Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    }
+  
+  if (data.hits[0].id && (page === 1 || page === 2)) {   
+      Notify.info(`Hooray! We found ${data.totalHits} images.`);
+   
+         }       
+    
+    const markupPicture = data.hits
+      .map((card) => {
+      return `<div class="photo-card">
+      <a class="link-card" href="${card.largeImageURL}">
+      <img src="${card.webformatURL}" alt="${card.tags}" loading="lazy" width="300" height="220"/>
+      <div class="info">
+        <p class="info-item">
+          <b>Likes</b>
+          <span>${card.likes}</span>
+        </p>
+        <p class="info-item">
+          <b>Views</b>
+         <span>${card.views}</span>
+        </p>
+        <p class="info-item">
+          <b>Comments</b>
+          <span>${card.comments}</span>
+        </p>
+        <p class="info-item">
+          <b>Downloads</b>
+          <span>${card.downloads}</span>
+        </p>
+      </div>
+      </a>
+    </div>`;    
+  })
+  .join("");
+  
+  gallery.insertAdjacentHTML('beforeend', markupPicture);
+
+      
+  
+  let lightbox = new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionDelay: 250,
+        });
+ lightbox.refresh();
+
+ const { height: cardHeight } = document
+ .querySelector('.gallery')
+ .firstElementChild.getBoundingClientRect();
+ 
+ window.scrollBy({
+ top: cardHeight * 2,
+ behavior: 'smooth',
+ });
   }
 
-if (data.hits[0].id && (page === 1 || page === 2)) {   
-    Notify.info(`Hooray! We found ${data.totalHits} images.`);
- 
-       }
-     
   
-  const markupPicture = data.hits
-    .map((card) => {
-    return `<div class="photo-card">
-    <img src="${card.webformatURL}" alt="${card.tags}" loading="lazy" width="300" height="220"/>
-    <div class="info">
-      <p class="info-item">
-        <b>Likes</b>
-        <span>${card.likes}</span>
-      </p>
-      <p class="info-item">
-        <b>Views</b>
-       <span>${card.views}</span>
-      </p>
-      <p class="info-item">
-        <b>Comments</b>
-        <span>${card.comments}</span>
-      </p>
-      <p class="info-item">
-        <b>Downloads</b>
-        <span>${card.downloads}</span>
-      </p>
-    </div>
-  </div>`;    
-})
-.join("");
-
-return gallery.insertAdjacentHTML('beforeend', markupPicture);
-}
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-load_more.addEventListener('click', () => {
-  const value = search_form.elements.searchQuery.value;
-    fetchPicture(value).then(showPictures);
-    page += 1;
-        
-    if (page > totalPages) {
-     load_more.classList.add('is-hidden');
-     Notify.info("We're sorry, but you've reached the end of search results.");
-    }
-    });
+ 
 
+
+
+
+ 
 
 
